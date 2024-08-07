c7b668c43594f6b223477dc5798420abd5fb7940
sagar-aute <sagaraute090@gmail.com>
2024-08-07 23:48:50 +0530

    Final Backend Project

---
+++ routes/index.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var router = express.Router();
+const userModel = require('./users');
+const postModel = require('./posts');
+const upload = require('./multer');
+
+const passport = require('passport');
+const localStrategy = require("passport-local");
+passport.use(new localStrategy(userModel.authenticate()));
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', {error: req.flash('error')});
+});
+
+router.get('/feed', isLoggedIn, async function(req, res) {
+  const user = await userModel.findOne({
+    username: req.session.passport.user
+  })
+  const posts = await postModel.find();
+  res.render('feed', {user, posts});
+});
+
+router.post('/upload', isLoggedIn, upload.single('file'), async function(req, res) {
+  if(!req.file){
+    return res.status(404).send("no files were given");
+  }
+  const user = await userModel.findOne({username: req.session.passport.user});
+  const post = await postModel.create({
+    image: req.file.filename,
+    imageText: req.body.filecaption,
+    user: user._id
+  });
+
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile");
+});
+
+router.get('/profile', isLoggedIn, async function(req, res) { 
+  const user = await userModel.findOne({
+    username: req.session.passport.user
+  }).populate("posts")
+  console.log(user);
+  res.render("profile", {user})
+});
+
+router.post('/register', function(req, res){
+  const userData = new userModel({
+    username: req.body.username, 
+    email: req.body.email, 
+    fullname: req.body.fullname
+  });
+
+  userModel.register(userData, req.body.password)
+  .then(function(){
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/profile");
+    })
+  })
+});
+
+router.post('/login', passport.authenticate("local", {
+  successRedirect: "/profile",
+  failureRedirect: "/login", 
+  failureFlash: true
+}),function(req, res){});
+
+router.get("/logout", function(req, res) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()) return next();
+  res.redirect("/login");
+}
+
+module.exports = router;
